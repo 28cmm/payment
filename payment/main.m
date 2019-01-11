0a962c9fa9eb43b0b90d7d06cd0d7c5173b5507f
28cmm <joshuafang1998@gmail.com>
2019-01-11 08:57:44 -0800

    Initial Commit

---
+++ payment/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  payment
+//
+//  Created by Yilei Huang on 2019-01-11.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
